e02484415b4543b609227ac6b2030ea9e635e162
bopomofo <Aboceder@gmail.com>
2023-11-25 15:05:52 +0800

    后端处理请求跨域问题

--- server/src/main/java/com/bopomofo/core/config/SecurityConfig.java
+++ server/src/main/java/com/bopomofo/core/config/SecurityConfig.java
@@ -10,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -42,7 +45,32 @@ public class SecurityConfig {
                 .logoutUrl("/api/member/logout") // 退出登录路径
                 .permitAll() // 放行退出登录接口
                 .and()
+                .cors()
+                .configurationSource(corsConfigurationSource()) // 放行请求跨域
+                .and()
                 .csrf().disable()
                 .build();
+    }
+
+    public CorsConfigurationSource corsConfigurationSource() {
+        //1.添加CORS配置信息
+        CorsConfiguration config = new CorsConfiguration();
+        //放行哪些原始域
+        config.addAllowedOrigin("*");
+        //是否发送Cookie信息
+        config.setAllowCredentials(true);
+        //放行哪些原始域(请求方式)
+        config.addAllowedMethod("*");
+        //放行哪些原始域(头部信息)
+        config.addAllowedHeader("*");
+        //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+        config.addExposedHeader("*");
+
+        //2.添加映射路径
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+
+        //3.返回新的configSource.
+        return configSource;
     }
 }
