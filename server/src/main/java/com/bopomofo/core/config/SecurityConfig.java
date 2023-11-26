451e43edb2fb60efedb6fb74404f24d90c7f09b4
bopomofo <Aboceder@gmail.com>
2023-11-26 15:05:47 +0800

    SpringSecurity RememberMe 功能

--- server/src/main/java/com/bopomofo/core/config/SecurityConfig.java
+++ server/src/main/java/com/bopomofo/core/config/SecurityConfig.java
@@ -11,9 +11,13 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -30,7 +34,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, PersistentTokenRepository repository) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> {
                     // auth.antMatchers("/path/**").permitAll(); // 如果需要放行其他请求，在上面写这个
@@ -50,11 +54,21 @@ public class SecurityConfig {
                 .cors(cors -> {
                     cors.configurationSource(corsConfigurationSource()); // 放行请求跨域
                 })
-                .csrf(AbstractHttpConfigurer::disable)
+                .rememberMe(remember -> {
+                    remember.alwaysRemember(false); // 是否勾选记住我，默认false
+                    remember.rememberMeParameter("remember-me"); // 表单提交字段名称，默认“remember-me”
+                    remember.rememberMeCookieName("remember-token"); // 记住我cookie的名称
+                    remember.tokenRepository(repository); // rememberMe持久化设置，自动保存到数据库中
+                    remember.tokenValiditySeconds(3600 * 24 * 7); // 有效期设置为7天
+                })
+                .csrf(AbstractHttpConfigurer::disable) // 关闭csrf
                 .build();
     }
 
-    public CorsConfigurationSource corsConfigurationSource() {
+    /**
+     * 跨域配置
+     */
+    private CorsConfigurationSource corsConfigurationSource() {
         //1.添加CORS配置信息
         CorsConfiguration config = new CorsConfiguration();
         //放行哪些原始域
@@ -74,5 +88,18 @@ public class SecurityConfig {
 
         //3.返回新的configSource.
         return configSource;
+    }
+
+    /**
+     * rememberMe持久化设置，自动保存到数据库中
+     * 用户调用logout方法后，或者token时间过期了，就会自动消除表中的对应数据
+     */
+    @Bean
+    public PersistentTokenRepository tokenRepository(DataSource dataSource) {
+        JdbcTokenRepositoryImpl repository = new JdbcTokenRepositoryImpl();
+        // 第一次运行生成表，后面就不需要运行了
+        // repository.setCreateTableOnStartup(true);
+        repository.setDataSource(dataSource);
+        return repository;
     }
 }
