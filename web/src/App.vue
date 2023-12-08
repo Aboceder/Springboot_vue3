ec8bbecc59bd91c847a64dd25f98eed1ff9c4b37
bopomofo <Aboceder@gmail.com>
2023-12-08 15:28:59 +0800

    通过路由，判断哪些页面需要添加公共header

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,11 +1,68 @@
 <script>
 
+import {defineComponent} from "vue";
+import {User} from "@element-plus/icons-vue";
+import {ElMessage} from "element-plus";
+
+export default defineComponent({
+    components: {User},
+    computed: {
+        hasHeader() {
+            let arr = ['/login', '/forgotPassword']
+            return arr.includes(this.$route.path)
+        }
+    },
+    data() {
+        return {
+            duration: 1500,
+        }
+    },
+    methods: {
+        logout() {
+            this.axios.post('/api/member/logout')
+                .then(response => {
+                    // 清除token
+                    this.Cookies.remove("access_token");
+
+                    // 弹窗提示
+                    ElMessage({message: response.data.data, type: 'success', duration: this.duration})
+
+                    // 延时跳转到登录页面
+                    setTimeout(() => {
+                        location.href = '/login';
+                    }, this.duration);
+                })
+        },
+    },
+})
 </script>
 
 <template>
+    <el-header v-if="!hasHeader">
+        <div class="div-header">
+            <div class="logo">
+                <el-icon>
+                    <User/>
+                </el-icon>
+            </div>
+            <div style="display: flex; justify-content: flex-end; flex: 1">
+                <el-button type="warning" @click="logout()">logout</el-button>
+            </div>
+        </div>
+    </el-header>
     <router-view/>
 </template>
 
 <style scoped>
+.div-header {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    border-bottom: 1px solid #DDDFE5;
+    padding: 0 20px;
+}
 
+.logo {
+    width: 200px;
+}
 </style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,38 +1,22 @@
 <template>
     <h1>hello {{ username }}</h1>
-    <el-button type="warning" @click="logout()">logout</el-button>
 </template>
 
 <script>
-import {ElMessage} from "element-plus";
+import {User} from "@element-plus/icons-vue";
 
 export default {
     name: "Home",
+    components: {User},
     created() {
         this.getUseInfo();
     },
     data() {
         return {
-            duration: 1500,
             username: ''
         }
     },
     methods: {
-        logout() {
-            this.axios.post('/api/member/logout')
-                .then(response => {
-                    // 清除token
-                    this.Cookies.remove("access_token");
-
-                    // 弹窗提示
-                    ElMessage({message: response.data.data, type: 'success', duration: this.duration})
-
-                    // 延时跳转到登录页面
-                    setTimeout(() => {
-                        location.href = '/login';
-                    }, this.duration);
-                })
-        },
         getUseInfo() {
             this.axios.get('/api/member/getUserInfo')
                 .then(response => {
@@ -48,28 +32,5 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-    /* 本例12个文字(加标点符号)；有多少个文字，width就是多少个em */
-    width: 14em;
-    /* 加上两个动画，一个是打字动画，使用steps让字一个一个的出现，
-     注意step和字数保持一致，光标动画也是同理，*/
-    animation: typingWords 5s steps(14) infinite, cursor 0.5s steps(1) infinite;
-    /* 要设置不允许换行，且溢出隐藏 */
-    white-space: nowrap;
-    overflow: hidden;
-    /* 使用右边框作为打印的指针光标 */
-    border-right: 1px solid #000;
-}
 
-@keyframes typingWords {
-    0% {
-        width: 0;
-    }
-}
-
-@keyframes cursor {
-    50% {
-        border-color: transparent;
-    }
-}
 </style>
